576125bab4e7e59e7eb9719919a1b7e4d6a17752
HideyasuTobaru <steganographi@gmail.com>
2015-07-06 21:42:34 +0900

    first commit

---
+++ LaughterOfAnimal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaughterOfAnimal
+//
+//  Created by tobaru on 2015/07/06.
+//  Copyright (c) 2015年 tobaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
