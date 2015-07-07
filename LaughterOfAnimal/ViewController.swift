83e72ebab8f07464e52bf11eda4ec8076dcc52c1
HideyasuTobaru <steganographi@gmail.com>
2015-07-07 22:42:09 +0900

    sample music play button

--- LaughterOfAnimal/ViewController.swift
+++ LaughterOfAnimal/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVAudioPlayerDelegate {
+    
+    var player : AVAudioPlayer!
+    var playButton : UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // NSBundleにファイルを配置(SupportFiles)
+        let soundFilePath : NSString = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        // ファイルパス
+        let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath as String)!
+        
+        // AVAudioPlayerのインスタンス生成
+        player = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+        
+        // AVAudioPlayerのデリゲートセット
+        player.delegate = self
+        
+        // button
+        playButton = UIButton()
+        playButton.frame.size = CGSizeMake(160, 60)
+        playButton.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+        playButton.setTitle("music start", forState: UIControlState.Normal)
+        playButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        playButton.backgroundColor = UIColor.grayColor()
+        playButton.addTarget(self, action: "onClicPlay:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(playButton)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func onClicPlay(sender: UIButton){
+        if player.playing == true {
+            player.stop()
+            sender.setTitle("start", forState: .Normal)
+        } else {
+            player.play()
+            sender.setTitle("stop", forState: .Normal)
+        }
+    }
 }
 
