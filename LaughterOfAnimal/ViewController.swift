f9eee7fc87be47164df3d67e0857169f64c0b5a2
HideyasuTobaru <steganographi@gmail.com>
2015-07-25 12:24:16 +0900

    add sample mp3 and play sound with notificationcenter

--- LaughterOfAnimal/ViewController.swift
+++ LaughterOfAnimal/ViewController.swift
@@ -9,21 +9,31 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVAudioPlayerDelegate {
     
     var playButton1 : UIButton!
     var playButton2 : UIButton!
-    var playButton3 : UIButton!
-    var playButton4 : UIButton!
-    var playButton5 : UIButton!
-    var playButton6 : UIButton!
-    var playButton7 : UIButton!
-    var playButton8 : UIButton!
+//    var playButton3 : UIButton!
+//    var playButton4 : UIButton!
+//    var playButton5 : UIButton!
+//    var playButton6 : UIButton!
+//    var playButton7 : UIButton!
+//    var playButton8 : UIButton!
 
-    
+    var player : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 音声ファイルパス取得 From Supporting Files
+        let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("sample1", ofType: "mp3")!)
+        
+        // プレイヤー準備
+        player = AVAudioPlayer(contentsOfURL: audioPath, error: nil)
+        
+        // AVAudioPlayerのデリゲートセット
+        player?.delegate = self
+
         
         let viewSizeWidth = self.view.frame.width/10
         let viewSizeHeight = self.view.frame.height/10
@@ -50,6 +60,10 @@ class ViewController: UIViewController {
         playButton2.backgroundColor = UIColor.grayColor()
         playButton2.addTarget(self, action: "onClicPlay2:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(playButton2)
+        
+        // 再生終了通知を監視
+        let nc = NSNotificationCenter.defaultCenter()
+        nc.addObserver(self, selector: "audioStopAction", name: "stop", object: nil)
 
     }
 
@@ -64,9 +78,46 @@ class ViewController: UIViewController {
     
     func onClicPlay1 (sender: UIButton) {
         println("onClickPlay1")
+        playSound(sender)
     }
     func onClicPlay2 (sender: UIButton) {
         println("onClickPlay2")
+    }
+    
+    func playSound(sender: UIButton) {
+        
+        
+        println(player.playing)
+        
+        if player.playing == true {
+            player.stop()
+            sender.setTitle("start", forState: .Normal)
+        } else {
+            player.play()
+            sender.setTitle("stop", forState: .Normal)
+            println("start sound")
+            println(player.playing)
+        }
+    }
+    
+    // plyaer が再生を終了した際にDelegateから本methodがcallされる
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+
+        println("play finish")
+        player.stop()
+
+        // 再生終了を通知
+        let noti = NSNotification(name: "stop", object: self)
+        NSNotificationCenter.defaultCenter().postNotification(noti)
+    }
+    
+    // 再生終了通知検知時処理
+    func audioStopAction() {
+        // 「Play」表示に戻す
+        //btnPlayPause!.setTitle("Play", forState: UIControlState.Normal)
+        println("audioStopAction")
+        playButton1.setTitle("music start1", forState: UIControlState.Normal)
+        
     }
     
     
